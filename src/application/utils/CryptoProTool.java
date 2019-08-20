edfff9a99912c6fa35081bf0241cbb49a1c9ac3e
ksergey <kagarmanovsv@gmail.com>
2019-08-20 15:00:34 +0300

    Commit cryptoPro first

--- src/application/utils/CryptoProTool.java
+++ src/application/utils/CryptoProTool.java
@@ -1,21 +1,26 @@
 package application.utils;
 
 import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.PrivateKey;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.UnrecoverableKeyException;
+
+import ru.CryptoPro.JCP.params.JCPProtectionParameter;
 
 public class CryptoProTool {
 
 	PrivateKey privateKey;
 	Signature sig;
-	
+
 	public void init() {
 		try {
 			Signature sig = Signature.getInstance("CryptoProSignature_2012_256", "JCP");
-			readKey();
+			readKey("", "");
 			sig.initSign(privateKey);
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
@@ -28,20 +33,29 @@ public class CryptoProTool {
 			e.printStackTrace();
 		}
 	}
-		
-		
-	private void readKey() {
-		
-	}
-	
-	public void sign() {
-		byte[] data = null;
+
+	private void readKey(String alias, String password) {
+		JCPProtectionParameter protParam;
+		KeyStore ks;
 		try {
-			sig.update(data);
-		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
+			ks = KeyStore.getInstance(KeyStore.getDefaultType());
+			privateKey = (PrivateKey) ks.getKey(alias, password.toCharArray());
+
+		} catch (KeyStoreException | UnrecoverableKeyException | NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
 	}
-}
 
+	public byte[] sign(byte[] data) {
+		try {
+			sig.update(data);
+			byte[] sign = sig.sign();
+			return sign;
+		} catch (SignatureException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	
+}
